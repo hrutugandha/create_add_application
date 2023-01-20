7051cc1fe29de5e10d2929c89a7a7b2e85a71678
Hrutugandha Nishigandh ✨✨✨ <62648846+hrutugandha@users.noreply.github.com>
2023-01-20 15:01:30 +0530

    created app.

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Navbar = () => {
+  return (
+    <NavbarWrapper>
+        1
+    </NavbarWrapper>
+  )
+}
+
+
+const NavbarWrapper = styled.div`
+     border: 1px solid red;
+     maxHeight: 200px;
+     maxWidth: 100%;
+
+`
+
+export default Navbar

78e2ffbcad0c9d401116dee7eca285969dfdfc1f
Hrutugandha Nishigandh ✨✨✨ <62648846+hrutugandha@users.noreply.github.com>
2023-02-01 14:43:15 +0530

    added doughnut chart

--- src/Components/DoughnutChart.jsx
+++ src/Components/DoughnutChart.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Doughnut } from "react-chartjs-2";
 
-const DoughnutChart = () => {
+function DoughnutChart() {
+
+ const data = {}
+
+ const options = {}
+
+ const plugins = [{
+     beforeDraw: function(chart) {
+      var width = chart.width,
+          height = chart.height,
+          ctx = chart.ctx;
+          ctx.restore();
+          var fontSize = (height / 160).toFixed(2);
+          ctx.font = fontSize + "em sans-serif";
+          ctx.textBaseline = "top";
+          var text = "Foo-bar",
+          textX = Math.round((width - ctx.measureText(text).width) / 2),
+          textY = height / 2;
+          ctx.fillText(text, textX, textY);
+          ctx.save();
+     } 
+   }]
+
+
+
   return (
-    <div>DoughnutChart</div>
-  )
+   
+        <Doughnut 
+          type="doughnut" 
+          data={data} 
+          options{...options} 
+          plugins={plugins} 
+         />
+  );
 }
 
-export default DoughnutChart
+export default DoughnutChart;
+
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <NavWrapper>
+   hi
+    </NavWrapper>
   )
 }
+
+
+const NavWrapper = styled.div`
+   height:100px;
+   border: 1px solid red
+   display: flex;
+   
+`
+
+
 
 export default Navbar
